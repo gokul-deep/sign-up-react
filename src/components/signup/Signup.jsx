f808734256a8048b0f3f5fd8b9520cde493634eb
GOKUL DEEP <125761507+gokul-deep@users.noreply.github.com>
2023-11-09 13:25:02 +0530

    Add files via upload

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,27 +1,24 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./Signup.css";
 import image from "../../images/logo-white3.png";
 
-class SignUpForm extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: "",
-      email: "",
-      password: "",
-    };
-  }
+const SignUpForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-  handleInputChange = (e) => {
+  const handleInputChange = (e) => {
     const { name, value } = e.target;
-    this.setState({ [name]: value });
+    setFormData({ ...formData, [name]: value });
   };
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const { name, email, password } = this.state;
+    const { name, email, password } = formData;
 
-    if (this.isFormValid()) {
+    if (isFormValid()) {
       const userData = {
         name,
         email,
@@ -33,8 +30,8 @@ class SignUpForm extends Component {
     }
   };
 
-  isFormValid = () => {
-    const { name, email, password } = this.state;
+  const isFormValid = () => {
+    const { name, email, password } = formData;
 
     const nameValid = name.length >= 4 && name.length <= 20;
     const emailValid = /\S+@\S+\.\S+/.test(email);
@@ -43,89 +40,87 @@ class SignUpForm extends Component {
     return nameValid && emailValid && passwordValid;
   };
 
-  render() {
-    const { name, email, password } = this.state;
-    const isFormValid = this.isFormValid();
+  const { name, email, password } = formData;
+  const isFormValidState = isFormValid();
 
-    return (
-      <div>
-        <div className="main container-fluid">
-          <div className="row">
-            <div className="col-lg-5 left">
-              <img src={image} alt="" className="logo" />
-              <div className="content">
-                <h2>Welcome Back!</h2>
-                <h5>
-                  To keep connected with us please login with your personal info
-                </h5>
-                <div className="button-container">
-                  <button className="btn btn-outline-light">SIGN IN</button>
-                </div>
+  return (
+    <div>
+      <div className="main container-fluid">
+        <div className="row">
+          <div className="col-lg-5 left">
+            <img src={image} alt="" className="logo" />
+            <div className="content">
+              <h2>Welcome Back!</h2>
+              <h5>
+                To keep connected with us please login with your personal info
+              </h5>
+              <div className="button-container">
+                <button className="btn btn-outline-light">SIGN IN</button>
               </div>
             </div>
-            <div className="col-lg-7 right">
-              <div className="content">
-                <h2>Create Account</h2>
-                <div className="social-container">
-                  <i className="fa-brands fa-facebook-f"></i>
-                  <i className="fa-brands fa-google-plus-g"></i>
-                  <i className="fa-brands fa-linkedin-in"></i>
-                </div>
-                <p>or use your email for registration :</p>
-                <form onSubmit={this.handleSubmit}>
-                  <div className="input-div">
-                    <input
-                      className="input-box"
-                      placeholder="Name"
-                      type="text"
-                      name="name"
-                      value={name}
-                      onChange={this.handleInputChange}
-                      required
-                    />
-                    <i className="fa-regular fa-user"></i>
-                  </div>
-                  <div className="input-div">
-                    <input
-                      className="input-box"
-                      placeholder="Email"
-                      type="email"
-                      name="email"
-                      value={email}
-                      onChange={this.handleInputChange}
-                      required
-                    />
-                    <i className="fa-regular fa-envelope"></i>
-                  </div>
-                  <div className="input-div">
-                    <input
-                      className="input-box"
-                      placeholder="Password"
-                      type="password"
-                      name="password"
-                      value={password}
-                      onChange={this.handleInputChange}
-                      required
-                    />
-                    <i className="fa-solid fa-lock"></i>
-                  </div>
-                  <div className="button-container">
-                    <button
-                      className="btn btn-success"
-                      type="submit"
-                      disabled={!isFormValid}
-                    >
-                      Sign Up
-                    </button>
-                  </div>
-                </form>
+          </div>
+          <div className="col-lg-7 right">
+            <div className="content">
+              <h2>Create Account</h2>
+              <div className="social-container">
+                <i className="fa-brands fa-facebook-f"></i>
+                <i className="fa-brands fa-google-plus-g"></i>
+                <i className="fa-brands fa-linkedin-in"></i>
               </div>
+              <p>or use your email for registration :</p>
+              <form onSubmit={handleSubmit}>
+                <div className="input-div">
+                  <input
+                    className="input-box"
+                    placeholder="Name"
+                    type="text"
+                    name="name"
+                    value={name}
+                    onChange={handleInputChange}
+                    required
+                  />
+                  <i className="fa-regular fa-user"></i>
+                </div>
+                <div className="input-div">
+                  <input
+                    className="input-box"
+                    placeholder="Email"
+                    type="email"
+                    name="email"
+                    value={email}
+                    onChange={handleInputChange}
+                    required
+                  />
+                  <i className="fa-regular fa-envelope"></i>
+                </div>
+                <div className="input-div">
+                  <input
+                    className="input-box"
+                    placeholder="Password"
+                    type="password"
+                    name="password"
+                    value={password}
+                    onChange={handleInputChange}
+                    required
+                  />
+                  <i className="fa-solid fa-lock"></i>
+                </div>
+                <div className="button-container">
+                  <button
+                    className="btn btn-success"
+                    type="submit"
+                    disabled={!isFormValidState}
+                  >
+                    Sign Up
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default SignUpForm;
